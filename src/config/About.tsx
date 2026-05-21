c5368aa6c6dfa2e88f1d1cf1ea74e78bc28e59b5
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-05-22 00:35:09 +0530

    add: landing & tech logos && app

---
+++ src/config/About.tsx
@@ -0,0 +1,22 @@
+import Javascript from "@/components/technologies/JavaScript";
+import MongoDB from "@/components/technologies/MongoDB";
+import NodeJS from "@/components/technologies/NodeJs";
+import ReactIcon from "@/components/technologies/ReactIcon";
+import TypeScript from "@/components/technologies/TypeScript";
+import PostgreSQL from "@/components/technologies/PostgreSQL";
+import NextJs from "@/components/technologies/NextJs";
+
+export const mySkills = [
+  <ReactIcon key="react" />,
+  <NodeJS key="nodejs" />,
+  <TypeScript key="typescript" />,
+  <Javascript key="javascript" />,
+  <MongoDB key="mongodb" />,
+  <PostgreSQL key="postgresql" />,
+  <NextJs key="nextjs" />,
+];
+
+export const about = {
+  name: "Devansh Singh Parmar",
+  description: `I love building things that live on the internet. I enjoy working on both the frontend and backend, and I'm always looking for new challenges to tackle.`,
+};
