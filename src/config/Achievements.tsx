9618a0999d8dcb279dda23c48002428257fcc7d3
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-05-23 22:25:32 +0530

    contact and cert

---
+++ src/config/Achievements.tsx
@@ -0,0 +1,13 @@
+export const certificates = [
+  {
+    file: "cert",
+    title: "I'll add certificates here",
+    issuer: "devansh",
+    date: "202ad",
+  },
+];
+
+const achievementsConfig = {
+  certificates,
+};
+export default achievementsConfig;
---
+++ src/config/Contact.tsx
@@ -0,0 +1,20 @@
+export const contactConfig = {
+  title: "Contact",
+  description: "Get in touch. I will try to get back as soon as I can.",
+  form: {
+    label: {
+      name: "Name",
+      email: "Email",
+      message: "Message",
+    },
+    placeholder: {
+      name: "Enter your name",
+      email: "Enter your email",
+      message: "Enter your message",
+    },
+    submitButton: "Send Message",
+    successMessage: "Your message has been sent successfully!",
+    errorMessage:
+      "There was an error sending your message. Please try again later.",
+  },
+};
