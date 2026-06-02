c8c4c5949ddc5300879a37e948c37b7266efe2b1
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-06-02 23:46:19 +0530

    footer

---
+++ src/components/common/Navbar.tsx
@@ -0,0 +1,7 @@
+import { navbarConfig } from "@/config/Navbar";
+import { Link } from "next-view-transitions";
+import Image from "next/image";
+import React from "react";
+import Container from "./Container";
+
+//need to add themeswitch so will come back later
