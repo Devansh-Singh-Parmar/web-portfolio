cd8ab90723c654579be17dfac692553614b6b530
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-06-09 23:40:11 +0530

    re:themeswitch

--- src/components/common/ThemeSwitch.tsx
+++ src/components/common/ThemeSwitch.tsx
@@ -117,8 +117,89 @@ export const useThemeToggle = ({
   };
 };
 
-export const ThemeToggleButton =({
-  className='',
-  ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-})
+export const ThemeToggleButton = ({
+  className = "",
+  variant = "circle",
+  start = "center",
+  blur = false,
+  gifUrl = "",
+}: {
+  className?: string;
+  variant?: AnimationVariant;
+  start?: AnimationStart;
+  blur?: boolean;
+  gifUrl?: string;
+}) => {
+  const { isDark, toggleTheme } = useThemeToggle({
+    variant,
+    start,
+    blur,
+    gifUrl,
+  });
+};
+return (
+  <Button
+    type="button"
+    variant="ghost"
+    size="icon"
+    className={cn(
+      "size-10 cursor-pointer p-0 transition-all duration-300 active:scale-95",
+      className,
+    )}
+    onClick={toggleTheme}
+    aria-label="Toggle Theme"
+  >
+    <span className="sr-only">Toggle theme</span>
+    {isDark ? <Moon className="size-4" /> : <Sun className="size-4" />}
+  </Button>
+);
+
+export type AnimationVariant =
+  | "circle"
+  | "rectangle"
+  | "gif"
+  | "polygon"
+  | "circle-blur";
+export type AnimationStart =
+  | "top-left"
+  | "top-right"
+  | "bottom-left"
+  | "bottom-right"
+  | "center"
+  | "top-center"
+  | "bottom-center"
+  | "bottom-up"
+  | "top-down"
+  | "left-right"
+  | "right-left";
+
+interface Animation {
+  name: string;
+  css: string;
+}
+
+const getPositionCoords = (position: AnimationStart) => {
+  switch (position) {
+    case "top-left":
+      return { cx: "0", cy: "0" };
+    case "top-right":
+      return { cx: "40", cy: "0" };
+    case "bottom-left":
+      return { cx: "0", cy: "40" };
+    case "bottom-right":
+      return { cx: "40", cy: "40" };
+    case "top-center":
+      return { cx: "20", cy: "0" };
+    case "bottom-center":
+      return { cx: "20", cy: "40" };
+    case "left-right":
+    case "bottom-up":
+    case "top-down":
+    case "right-left":
+      return { cx: "20", cy: "20" };
+  }
+};
+
+const generateSVG = (variant: AnimationVariant, start: AnimationStart) => {
+  if (variant === "gif") return "";
+};
