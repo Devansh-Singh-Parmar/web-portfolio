6ac3a9f514bc769d627e3ec44f431db08fcf8177
Devansh Singh Parmar <devansh18dp@gmail.com>
2026-06-23 18:08:02 +0530

    oneko and quote

---
+++ src/components/common/OnekoCat.tsx
@@ -0,0 +1,10 @@
+import { catConfig } from "@/config/Cat";
+import Script from "next/script";
+import React from "react";
+
+export default function OnekoCat() {
+  if (!catConfig.enabled) {
+    return null;
+  }
+  return <Script src="./oneko/oneko.gif" data-cat="./oneko/oneko.gif" />;
+}
